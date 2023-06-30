6086fcfb7b263c3b0b63da74c58b7a51f6812304
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2023-06-29 22:06:27 -0500

    optimizar todoReducer

--- src/components/09-useReducer/todoReducer.js
+++ src/components/09-useReducer/todoReducer.js
@@ -5,7 +5,7 @@ export const todoReducer = (state = [], action) => {
             return [...state, action.payload];
         case 'delete':
             return state.filter( todo => todo.id !== action.payload);
-        case 'toggle':
+        case 'toggle-old':
             return state.map(todo => {
                 if (todo.id === action.payload) {
                     return {...todo, done: !todo.done};
@@ -13,6 +13,10 @@ export const todoReducer = (state = [], action) => {
                     return todo;
                 }
             });
+        case 'toggle':
+            return state.map(todo =>  ( todo.id === action.payload 
+                                        ? {...todo,done:!todo.done}
+                                        : todo)  );
 
         default:
             return state;
