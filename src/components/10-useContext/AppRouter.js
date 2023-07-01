3d1a222be385d4575fce38af18bc4ce697295977
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2023-07-01 12:14:01 -0500

    Link y NavLink

--- src/components/10-useContext/AppRouter.js
+++ src/components/10-useContext/AppRouter.js
@@ -3,20 +3,22 @@ import {
     BrowserRouter,
     Routes,
     Route,
-    Link,
+    Navigate,
   } from "react-router-dom";
 import { AboutScreen } from './AboutScreen';
 import { LoginScreen } from './LoginScreen';
 import { HomeScreen } from './HomeScreen';
+import { NavBar } from './NavBar';
 
 export const AppRouter = () => {
   return (
    <BrowserRouter>
+   <NavBar></NavBar>
    <Routes>
     <Route exact path='/about'element={<AboutScreen />}></Route>
     <Route exact path='/login'element={<LoginScreen />}></Route>
     <Route exact path='/'element={<HomeScreen />}></Route>
-
+    <Route path='*' element={<Navigate to = "/" replace/>}></Route>
    </Routes>
    </BrowserRouter>
   );
