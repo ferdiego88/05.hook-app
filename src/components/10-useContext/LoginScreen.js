9bce0bd1ff2b1ae81ab9035bc566d6a8f10faef2
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2023-07-03 19:52:18 -0500

    useContext ejemplo

--- src/components/10-useContext/LoginScreen.js
+++ src/components/10-useContext/LoginScreen.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from './UserContext';
 
 export const LoginScreen = () => {
+  
+  const user = {
+    id: 123,
+    name: 'Fernando',
+  }
+  const { setUser } = useContext(UserContext);
   return (
     <div>
         <h1>LoginScreen</h1>
         <hr />
+        <button 
+          className='btn btn-primary'
+          onClick={() =>setUser(user)}
+          >
+          Login
+        </button>
     </div>
   )
 }
